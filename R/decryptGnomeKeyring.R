fdd1f25cb9c076f14e0c5f30c9fd17025534c748
Josh Gilfillan <j.gilfillan@gmail.com>
2016-10-23 06:06:02 +1000

    first cran submission

--- R/decryptGnomeKeyring.R
+++ R/decryptGnomeKeyring.R
@@ -1,11 +1,11 @@
 #' Get a password from Gnome Keyring using secret-tool
 #'
-#' @param key_value_pairs A list of key value pairs as expected by secret-tool (see example)
+#' @param key_value_pairs A string of key value pairs as expected by secret-tool (see example)
 #' @return A decrypted password as a string.
 #' @examples
 #' \dontrun{
 #' # returns the password for the key-value pair key: MYSCHEMA_MYDB
-#' decryptGnomeKeyring(key MYSCHEMA_MYDB)
+#' decryptGnomeKeyring("key MYSCHEMA_MYDB")
 #' }
 #' @export
 decryptGnomeKeyring <- function(key_value_pairs) {
