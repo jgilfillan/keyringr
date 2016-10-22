2de3c4f2cad0e01fada7418c352214ac50c01e1b
Josh Gilfillan <j.gilfillan@gmail.com>
2016-10-23 05:29:47 +1000

    added man page

--- R/decryptGnomeKeyring.R
+++ R/decryptGnomeKeyring.R
@@ -3,8 +3,12 @@
 #' @param key_value_pairs A list of key value pairs as expected by secret-tool (see example)
 #' @return A decrypted password as a string.
 #' @examples
-#' add(1, 1)
+#' \dontrun{
+#' # returns the password for the key-value pair key: MYSCHEMA_MYDB
+#' decryptGnomeKeyring(key MYSCHEMA_MYDB)
+#' }
 #' @export
 decryptGnomeKeyring <- function(key_value_pairs) {
   return(system(paste("secret-tool lookup ", key_value_pairs, sep="")))
 }
+
