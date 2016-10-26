83ca0f966a61ec6825c841521988d527283173c8
Josh Gilfillan <j.gilfillan@gmail.com>
2016-10-27 06:39:16 +1000

    updated documentation

--- R/decrypt_dpapi_pw.R
+++ R/decrypt_dpapi_pw.R
@@ -1,5 +1,29 @@
 #' Decrypt passwords encrypted with  the Microsoft Data Protection API
 #' @param file File that holds a password encrypted using DPAPI
+#' @return An decrypted password as an invisible string.  Invisible means that
+#' the password won't be displayed in the console, but can be assigned to a
+#' variable or used inline.
+#' @details
+#' Requires Powershell to be installed and execution policy set to RemoteSigned.
+#' This can be achieved by running
+#' \code{
+#' Set-ExecutionPolicy RemoteSigned
+#' }
+#' from Powershell.
+#' @examples
+#' \dontrun{
+#' # First run the command below from Powershell:
+#' # Read-Host "Enter Password" -AsSecureString |  ConvertFrom-SecureString | Out-File "C:\Temp\Password.txt"
+#' # Now execute the following R commands to decrypt the password and save it in
+#' # variable "x".  Note that if run without assignment, the password will not
+#' # be displayed in the console.  Passwords must be saved to a variable or used
+#' # inline within a connection string.
+#' library("keyringr")
+#' x <- decrypt_dpapi_pw("C:\\Temp\\Password.txt")
+#'
+#' # function is best used in a connection string command:
+#' ch <- odbcConnect("some dsn", uid = "user1", pwd = decrypt_dpapi_pw("C:\\Temp\\Password.txt"))
+#' }
 #' @export
 decrypt_dpapi_pw <- function(file) {
   stopifnot(.Platform$OS.type == "windows")
