a0bb080d91b908aa623b96b32bb1324b5d75aed2
user122416 <user122416@103-246-99-71.moboware.com>
2016-11-21 12:52:43 -0800

    added keychain support

--- R/decrypt_kc_pw.R
+++ R/decrypt_kc_pw.R
@@ -1,8 +1,15 @@
-#' Get a password from OSX Keyring using 'security' cli
+#' Get a generic password from OSX Keychain using the 'security' cli
 #' @param label Keychain password label
-#' @return An decrypted password as an invisible string.  Invisible means that
+#' @param type Leychain password type.  Either "generic" or "internet".
+#' @return Returns a decrypted password as an invisible string.  Invisible means that
 #' the password won't be displayed in the console, but can be assigned to a
 #' variable or used inline.
+#' @details
+#' Passwords must be saved in Keychain prior to using the function.
+#'
+#' OSX may require the user to grant access to "security" the first time the function
+#' is run for each password.  It is important to select "Always allow", which will
+#' prevent similar dialogs in the future.
 #' @examples
 #' \dontrun{
 #' # First store a password in Keychain
@@ -13,11 +20,10 @@
 #' ch <- odbcConnect("some dsn", uid = "user1", pwd = decrypt_kc_pw("mydb_myuser"))
 #' }
 #' @export
-decrypt_kc_pw <- function(account) {
+decrypt_kc_pw <- function(label, type = "generic") {
   # test OS is Linux
   stopifnot(Sys.info()["sysname"] == "Darwin")
+  stopifnot((type %in% c("generic","internet")))
   # execute security command and invisible return result
-  invisible(system(paste("security 2>&1 >/dev/null find-generic-password -ga ", account, " | grep '^password' | cut -c 12- | rev | cut -c 2- | rev", sep=""), intern=TRUE))
+  invisible(system(paste("security 2>&1 >/dev/null find-", type,"-password -gl ", label, " | grep '^password' | cut -c 12- | rev | cut -c 2- | rev", sep=""), intern=TRUE))
 }
-
-# security 2>&1 >/dev/null find-generic-password -ga dwstats_dwpd | grep '^password' | cut -c 12- | rev | cut -c 2- | rev
